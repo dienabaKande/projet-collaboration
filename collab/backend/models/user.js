14ae4fd933428f14c4ea450bb777b44f874945cb
Kine Ndiaye <ndiayefatoukine232@gmail.com>
2025-07-03 21:51:55 +1400

    Fusion des dossiers backend et frontend dans la racine

--- collab/backend/models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  nom: { type: String, required: true },
-  droits: [String]
-});
-
-module.exports = mongoose.model('User', userSchema);
--- models/User.js
+++ models/User.js
@@ -1,13 +1,8 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  projects: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Project' }],
-    projects: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Project',
-    }],
-  });
-module.exports = mongoose.model('User', UserSchema);
+const userSchema = new mongoose.Schema({
+  nom: { type: String, required: true },
+  droits: [String]
+});
+
+module.exports = mongoose.model('User', userSchema);
